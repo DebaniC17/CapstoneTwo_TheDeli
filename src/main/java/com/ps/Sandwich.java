bfd108076d51b45012b165073e90a149bbb65251
Debani <debani.cossio1217@gmail.com>
2024-11-10 17:11:33 -0600

    worked on sandwich class

--- src/main/java/com/ps/Sandwich.java
+++ src/main/java/com/ps/Sandwich.java
@@ -1,4 +1,52 @@
 package com.ps;
 
-public class Sandwich {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sandwich extends Product {
+    private int size;
+    private String breadType;
+    private boolean isToasted;
+    private List<Topping> toppings;
+
+    public Sandwich(int size, String breadType, boolean isToasted) {
+        this.size = size;
+        this.breadType = breadType;
+        this.isToasted = isToasted;
+        this.toppings = new ArrayList<>();
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getBreadType() {
+        return breadType;
+    }
+
+    public void setBreadType(String breadType) {
+        this.breadType = breadType;
+    }
+
+    public boolean isToasted() {
+        return isToasted;
+    }
+
+    public void setToasted(boolean toasted) {
+        isToasted = toasted;
+    }
+    public List<Topping> getToppings() {
+        return toppings;
+    }
+    public void addTopping(Topping topping) {
+        toppings.add(topping);
+    }
+    public void removeTopping(Topping topping) {
+        toppings.remove(topping);
+    }
+    //get price override method
 }
