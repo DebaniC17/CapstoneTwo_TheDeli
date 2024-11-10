3215b24f01f0b2c5fb5665eb25cc0a9edcc5b21d
Debani <debani.cossio1217@gmail.com>
2024-11-10 03:50:35 -0600

    worked on drink class

--- src/main/java/com/ps/Drink.java
+++ src/main/java/com/ps/Drink.java
@@ -1,4 +1,44 @@
 package com.ps;
 
-public class Drink {
+public class Drink extends Product{
+    private String flavor;
+    private String size;
+
+    public Drink(String flavor, String size) {
+        this.flavor = flavor;
+        this.size = size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+    public double getPrice() {
+        double price;
+
+        if (size.equalsIgnoreCase("small")) {
+            price = 1.50;
+        } else if (size.equalsIgnoreCase("medium")) {
+            price = 2.00;
+        } else if (size.equalsIgnoreCase("large")) {
+            price = 2.50;
+        } else {
+            price = 0.0;
+            System.out.println("Unknown size: ");
+        }
+        return price;
+
+    }
+
 }
